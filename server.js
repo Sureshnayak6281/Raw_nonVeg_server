4aaf35bb2f82498ac03cdc0c328a13198dd4c4cd
Banothu Suresh <sureshnayak6281@gmail.com>
2024-10-16 14:02:10 +0530

    chanes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // or your frontend URL
+  origin: ['http://localhost:3000', 'https://preeminent-empanada-733ca8.netlify.app'],
   credentials: true
 }));
 
